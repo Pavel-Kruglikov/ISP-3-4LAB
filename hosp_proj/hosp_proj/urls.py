3e49f9167e399a3f63453e942810d57e249f1fc9
Pavel_9 <pavel.kruglikov.95@gmail.com>
2022-05-24 19:19:26 +0300

    databases
    starting to work with databases

--- hosp_proj/hosp_proj/urls.py
+++ hosp_proj/hosp_proj/urls.py
@@ -1,4 +1,4 @@
-"""taskmanager URL Configuration
+"""hosp_proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
